90b764a8724270d6a533fc8ccc3f528ee7d8f223
Constantine Nicolaou <constantin.nicolaou@gmail.com>
2012-12-17 00:59:38 +0200

    Added blogger application files to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = 'c6a9401d402aee7cac12c3c34e0b4abf6ee87466293a5814ce6616efae5e8cfd4880d36337883bd32e600640c4fee5fc9eae5e25dfb3e551b32ba313a2105ef9'
